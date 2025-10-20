469ccb2610af7fd35756cfe258193610d9209332
vh <180421300+rdgl@users.noreply.github.com>
2025-10-20 21:17:01 +0530

    Add comment route to post.routes.js

--- social/server/routes/post.routes.js
+++ social/server/routes/post.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { uploadPost , getAllPosts  , like} from '../controllers/post.controllers.js'
+import { uploadPost , getAllPosts  , like, comment } from '../controllers/post.controllers.js'
 import { upload } from '../middlewares/multer.js'
 import isAuth from '../middlewares/isAuth.js'
 
@@ -11,7 +11,7 @@ const postRouter = express.Router()
 postRouter.post('/uploadPost',isAuth , upload.single('mediaUrl'), uploadPost )
 postRouter.get("/getAllPosts" , isAuth ,getAllPosts );
 postRouter.post("/like/:postId", isAuth, like)
-
+postRouter.post("/comment/:postId", isAuth, comment)
 
 
 export default postRouter
